71624feaaa8bc5a65d2470aa44c5d95956e1e44f
Udbhav Awasthi <udbhavawasthi7@gmail.com>
2025-11-07 16:09:17 +0530

    Add files via upload

---
+++ src/main/java/com/example/demo/KnowyourmoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowyourmoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowyourmoodApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/demo/entity/MoodType.java
@@ -0,0 +1,11 @@
+package com.example.demo.entity;
+
+public enum MoodType {
+    HAPPY,
+    SAD,
+    ANGRY,
+    CALM,
+    EXCITED,
+    STRESSED,
+    NEUTRAL
+}
